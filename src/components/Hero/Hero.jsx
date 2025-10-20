030f7240855aee1ada81cea9ee44973592cfcd5c
TazeemTayob17 <tazeemtayob17@gmail.com>
2025-10-20 16:11:58 +0200

    Download CV functionality implemented

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import heroImage from "../../assets/hero/heroImage.png";
 import styles from "./Hero.module.css";
+import CV from "../../assets/CV.pdf";
 
 function Hero() {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Tazeem</h1>
+        <h1 className={styles.title}>Hi, I'm Tazeem Tayob</h1>
         <p className={styles.description}>
           I'm a BSc Computer Science and Mathematics student at University of
           the Witwatersrand
         </p>
-        <a href="mailto:tazeemtayob17@gmail.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.actions}>
+          <a href={CV} download className={styles.CVbtn}>
+          Download CV
+          </a>
+          <a href="mailto:tazeemtayob17@gmail.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
+        </div>
       </div>
       <img src={heroImage} alt="hero image of me" className={styles.heroImg} />
       <div className={styles.topBlur} />
