9f1fbc9e28a9d8a48685243e514f4df775b830ec
TazeemTayob17 <tazeemtayob17@gmail.com>
2025-10-21 14:57:55 +0200

    App layout change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import styles from "./App.module.css";
-import  About from "./components/About/About.jsx";
-import  Contact  from "./components/Contact/Contact.jsx";
-import  Skills from "./components/Skills/Skills.jsx";
-import  Hero from "./components/Hero/Hero.jsx";
-import  Navbar from "./components/Navbar/Navbar.jsx";
-import  Projects from "./components/Projects/Projects.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Skills from "./components/Skills/Skills.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Projects from "./components/Projects/Projects.jsx";
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Skills />
       <Projects />
+      <Skills />
       <Contact />
     </div>
   );
