d588a9ccc4cda6d0372c298f4a41e0274cf0a4ea
TazeemTayob17 <tazeemtayob17@gmail.com>
2025-10-23 12:44:43 +0200

    Projects.jsx update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,9 @@ function Projects() {
   //console.log(getImageUrl(imageSrc));
   return (
     <section className={styles.container} id="projects">
-      <h2 className={styles.title}>Projects</h2>
+      <h2 className={styles.title}>Projects</h2> 
+      <p className={styles.projectsIntro}>The following projects showcase my skills and experience through examples of my work. <br/>
+        Each project is briefly described with links to code repositories in it.</p>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
